95b2a8238b11e493086831ef4fb628225a2f1dc2
GeGe-K <gloriagivondo@gmail.com>
2018-10-28 21:41:15 +0300

    generated a date-count pipe.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { SearchService } from './search.service';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,

a85371fac397cdbb71f117ca671eb3ac8e39fb89
GeGe-K <gloriagivondo@gmail.com>
2018-10-29 09:14:36 +0300

    used inline styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-// import { NgProgressModule } from '@ngx-progressbar/core';
-// import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 import { SearchService } from './search.service';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -20,9 +21,10 @@ import { SearchFormComponent } from './search-form/search-form.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
-    // NgProgressModule.forRoot(),
-    // NgProgressHttpModule.forRoot()
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule.forRoot()
   ],
   providers: [SearchService],
   bootstrap: [AppComponent]

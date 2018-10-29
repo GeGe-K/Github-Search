eed628ab56e3fcc5e90bff7c73f69a7191fb5695
GeGe-K <gloriagivondo@gmail.com>
2018-10-29 06:55:41 +0300

    generated a class search.

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SearchService } from '../search.service';
+impo
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
+  newSearch = new Search ('');
 
-  constructor() { }
+  submitUser(name){
+    this.search.getUserInfo(name.val);
+    this.search.getRepoInfo(name.val);
+  }
+  constructor(private search:SearchService) { }
 
   ngOnInit() {
   }

f932be22c944e9b5e9ee5e184a2dcde35eb75265
GeGe-K <gloriagivondo@gmail.com>
2018-11-01 08:02:36 +0300

    Auto-generated commit

--- src/app/repo.ts
+++
@@ -1,3 +0,0 @@
-export class Repo {
-    constructor(public name:string, public description: string, public created_on:Date, public updated_on:Date, public html_url:string, public svn_url:string, public homepage:string){}
-}
--- src/app/user.ts
+++
@@ -1,3 +0,0 @@
-export class User {
-    constructor(public login:string, public name:string, public avatar_url:string, public bio:string, public followers:number, public following: number, public repos:number, public email:string, public html_url:string, public svn_url:string, public created_on:Date ){}
-}
